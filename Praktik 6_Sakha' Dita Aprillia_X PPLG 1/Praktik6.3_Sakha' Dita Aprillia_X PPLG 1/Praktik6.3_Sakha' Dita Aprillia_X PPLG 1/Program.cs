9cdb9a888c1a9fe130d56e462e620fb65b0cc5e2
SakhaDitaAprillia <sakha258731@student.smkn1kandeman.sch.id>
2025-11-24 08:54:47 +0700

    Add files via upload

---
+++ Praktik 6_Sakha' Dita Aprillia_X PPLG 1/Praktik6.3_Sakha' Dita Aprillia_X PPLG 1/Praktik6.3_Sakha' Dita Aprillia_X PPLG 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik6._3_Sakha__Dita_Aprillia_X_PPLG_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nama = new string[3];
+            int[] nilai = new int[3];
+
+            Console.Write("Masukkan nama siswa ke-1: ");
+            nama[0] = Console.ReadLine();
+            Console.Write("Masukkan nilai ke-1: ");
+            nilai[0] = int.Parse(Console.ReadLine());
+
+            Console.Write("Masukkan nama siswa ke-2: ");
+            nama[1] = Console.ReadLine();
+            Console.Write("Masukkan siswa ke-2: ");
+            nilai[1] = int.Parse(Console.ReadLine());
+
+            Console.Write("Masukkan nama siswa ke-3: ");
+            nama[2] = Console.ReadLine();
+            Console.Write("Masukkan nilai siswa ke-3: ");
+            nilai[2] = int.Parse(Console.ReadLine());
+        }
+    }
+}
