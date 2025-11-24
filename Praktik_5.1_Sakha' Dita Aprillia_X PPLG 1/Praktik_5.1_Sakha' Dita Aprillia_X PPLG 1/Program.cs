0caf316095a9e199889747db2589263eaa16584f
SakhaDitaAprillia <sakha258731@student.smkn1kandeman.sch.id>
2025-11-24 08:45:58 +0700

    Add files via upload

---
+++ Praktik_5.1_Sakha' Dita Aprillia_X PPLG 1/Praktik_5.1_Sakha' Dita Aprillia_X PPLG 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik_5._1_Sakha__Dita_Aprillia_X_PPLG_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Perulangan for dimulai dari sini 
+            for (int i = 1; i <= 10; i++)
+            {
+                // Blok kode yang akan diulang
+                Console.WriteLine("Angka ke-" + i);
+            }
+
+                Console.WriteLine("\nPerulangan selesai.");
+        }
+    }
+}
---
+++ Praktik_5.3_Sakha' Dita Aprillia_X PPLG 1/Praktik_5.3_Sakha' Dita Aprillia_X PPLG 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik_5._3_Sakha__Dita_Aprillia_X_PPLG_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputUser = "";
+
+            Console.WriteLine("Program akan terus berjalan hingga Anda mengetik 'keluar'.");
+
+            while (inputUser.ToLower() != "keluar")
+            {
+                Console.WriteLine("\nKetik sesuatu (atau 'keluar' untuk berhenti): ");
+                inputUser = Console.ReadLine();
+
+                Console.WriteLine("Anda mengetik: " + inputUser);
+            }
+                Console.WriteLine("\nProgram selesai. Terimakasih!");
+        }
+    }
+}
