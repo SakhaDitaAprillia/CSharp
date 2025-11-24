2491409bf07b4417827298cbda88be928b3ad6c5
SakhaDitaAprillia <sakha258731@student.smkn1kandeman.sch.id>
2025-11-24 07:25:30 +0700

    Add files via upload

---
+++ Latihan1_Sakha' Dita Aprillia_X PPLG 1/Latihan1_Sakha' Dita Aprillia_X PPLG 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan1_Sakha__Dita_Aprillia_X_PPLG_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int angka = 5;
+
+            for (int i = 1; i <= 10;  i++)
+            {
+                Console.WriteLine(angka + " X " + i + " = " + (angka * i));
+            }
+        }
+    }
+}
---
+++ Latihan2_Sakha' Dita Aprillia_X PPLG 1/Latihan2_Sakha' Dita Aprillia_X PPLG 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan2_Sakha__Dita_Aprillia_X_PPLG_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int total = 0;
+
+            while (i <= 5)
+            {
+                total += i;
+                i++;
+            }
+
+            Console.WriteLine("Total: " + total);
+        }
+    }
+}
