f0f0bb2ca52d27116982b13377db279904442f9c
SakhaDitaAprillia <sakha258731@student.smkn1kandeman.sch.id>
2025-11-24 08:52:11 +0700

    Add files via upload

---
+++ Praktik_5.4_Sakha' Dita Aprillia_X PPLG 1/Praktik_5.4_Sakha' Dita Aprillia_X PPLG 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik_5._4_Sakha__Dita_Aprillia_X_PPLG_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hitungan = 5;
+
+            Console.WriteLine("Memulai hitungan mundur:");
+
+            while (hitungan > 0)
+            {
+                Console.WriteLine($"Hitungan: {hitungan}");
+
+                hitungan = hitungan - 1;
+            }
+            Console.WriteLine("Selesai Hiitungan mundur telah berakhir.");
+        }
+    }
+}
