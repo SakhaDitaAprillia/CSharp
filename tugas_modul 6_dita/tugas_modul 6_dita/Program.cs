d0fc06050eec0d5a6b8b176cfd70da7eed3d2703
SakhaDitaAprillia <sakha258731@student.smkn1kandeman.sch.id>
2025-11-24 08:59:28 +0700

    Add files via upload

---
+++ tugas_modul 6_dita/tugas_modul 6_dita/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas_modul_6_dita
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[,] siswa = new string[3, 3]
+            {
+                { "Andi", "L", "X IPA 1" },
+                { "Bunga", "P", "X IPS 2" },
+                { "Cahyo", "L", "XI IPA 3" }
+            };
+
+            Console.WriteLine("Data Siswa: ");
+            Console.WriteLine("Nama\tJenis Kelamin\tKelas");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"{siswa[i, 0]}\t{siswa[i, 1]}\t\t{siswa[i, 2]}");
+            }
+        }
+    }
+}
---
+++ tugas_modul 7_dita/tugas_modul 7_dita/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas_modul_7_dita
+{
+    internal class Program
+    {
+        static void Kali(int a, int b, int c)
+        {
+            int hasil = a * b * c;
+            Console.WriteLine("Hasil perkalian: " + hasil);
+        }
+        static double RataRata(double a, double b, double c)
+        {
+            return (a + b + c) / 3;
+        }
+        static double KonversiSuhu(double celcius)
+        {
+            return (celcius * 9 / 5) + 32;
+        }
+        static void Main(string[] args)
+        {
+            Kali(2, 3, 4);
+
+            double rata = RataRata(70, 80, 90);
+            Console.WriteLine("Rata-rata: " + rata);
+
+            double suhuCelcius = 25;
+            double suhuFarenheit = KonversiSuhu(suhuCelcius);
+            Console.WriteLine($"{suhuCelcius}℃ = {suhuFarenheit}℉");
+        }
+    }
+}
